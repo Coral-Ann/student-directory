c4b4b137eb2deeda986678aa6b0b69994bbb2c07
Coral <coral.baker@outlook.com>
2022-03-18 16:46:22 +0000

    Added option to load students.csv to directory.rb.

--- lib/directory.rb
+++ lib/directory.rb
@@ -1,4 +1,4 @@
-# Refactoring menu.
+# Adding load to program.
 @students = []
 
 def input_students
@@ -24,7 +24,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv" # Added new option to save file.
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because I'm adding more options later.
 end
 
@@ -35,15 +36,24 @@ def show_students
 end
 
 def save_students
-  # Open the file for writing.
   file = File.open("students.csv","w")
-  # Iterate through the array of students.
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
   end
   file.close
+  puts "Saved directory, returning to menu."
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",") # Parallel assignment.
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close # Closing file.
+  puts "Loaded directory, returning to menu."
 end
 
 def process(selection)
@@ -52,8 +62,10 @@ def process(selection)
       input_students
     when "2"
       show_students
-    when "3" # Connecting the input '3' to the new method.
+    when "3" 
       save_students
+    when "4" # Connecting the input '4' to the new method.
+      load_students
     when "9"
       exit
     else
