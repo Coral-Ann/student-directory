38338f936b16093b9be4cb91f7349ccedf50d7cf
Coral <coral.baker@outlook.com>
2022-03-18 17:23:31 +0000

    Changed load to happen on program opening.

--- lib/directory.rb
+++ lib/directory.rb
@@ -1,59 +1,19 @@
-# Adding load to program.
+# Loading file on program opening, plus file if argument given when running program in ruby.
 @students = []
-
-def input_students
-  puts "Please enter the names of the students."
-  puts "To finish, just hit enter twice."
-  name = gets.chomp
-
-  while !name.empty? do
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students."
-    name = gets.chomp
-  end
-  @students
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
 
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
-  puts "9. Exit" # 9 because I'm adding more options later.
+  puts "9. Exit"
 end
 
-def show_students
-  print_header
-  print_student_list
-  print_footer
-end
-
-def save_students
-  file = File.open("students.csv","w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp) # Changed all 'gets' to start with STDIN, to clarify input stream.
   end
-  file.close
-  puts "Saved directory, returning to menu."
-end
-
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(",") # Parallel assignment.
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close # Closing file.
-  puts "Loaded directory, returning to menu."
 end
 
 def process(selection)
@@ -64,13 +24,32 @@ def process(selection)
       show_students
     when "3" 
       save_students
-    when "4" # Connecting the input '4' to the new method.
+    when "4"
       load_students
     when "9"
       exit
     else
       puts "I don't know what you meant, try again."
   end
+end
+
+def input_students
+  puts "Please enter the names of the students."
+  puts "To finish, just hit enter twice."
+  name = STDIN.gets.chomp
+
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students."
+    name = STDIN.gets.chomp
+  end
+  @students
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
 end
 
 def print_header
@@ -88,4 +67,38 @@ def print_footer
   puts "Overall, we have #{@students.count} great students."
 end
 
+def save_students
+  file = File.open("students.csv","w") 
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+  puts "Saved directory, returning to menu."
+end
+
+def load_students(filename = "students.csv") # If no argument is given, 'students.csv' is the default file to open.
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+  puts "Loaded directory, returning to menu."
+end
+
+def try_load_students
+  filename = ARGV.first # First argument from the command line.
+  return if filename.nil? # Get out of the method if it isn't supplied.
+  if File.exists?(filename) # If it does exist.
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # If it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
