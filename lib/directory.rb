1bf1b742c20a89d73c4d189ca81cc2667dea0ef2
Coral <coral.baker@outlook.com>
2022-03-18 15:48:48 +0000

    Refactored directory.rb into more methods.

--- lib/directory.rb
+++ lib/directory.rb
@@ -1,17 +1,49 @@
-# Adding an interactive menu.
+# Refactoring menu.
+@students = []
 
 def input_students
   puts "Please enter the names of the students."
   puts "To finish, just hit enter twice."
-  students = []
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students."
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students."
     name = gets.chomp
   end
-  students
+  @students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because I'm adding more options later.
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again."
+  end
 end
 
 def print_header
@@ -19,39 +51,14 @@ def print_header
   puts "-------------"
 end
 
-def print(names)
-  names.each do |name|
-    puts "#{name[:name]} (#{name[:cohort]} cohort)"
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students."
-end
-
-def interactive_menu
-  students = []
-  loop do
-    # 1. Print the menu and ask the user what to do.
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because I'm adding more options later.
-    # 2. Read the input and save it into a variable.
-    selection = gets.chomp
-    # 3. Do what the user asks.
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # Exit the program.
-    else
-      puts "I don't know what you meant, try again."
-    end
-  end
+def print_footer
+  puts "Overall, we have #{@students.count} great students."
 end
 
 interactive_menu
